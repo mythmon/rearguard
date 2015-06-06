4f8565516846321853139c10ec6dbe9f389abcf9
Mike Cooper <mythmon@gmail.com>
2015-06-05 22:23:28 -0700

    Client broadcast.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,108 @@
+#![feature(std_misc)]
+
 use std::io::{BufReader, BufRead, Result, Write};
 use std::net::{self, TcpListener, TcpStream};
 use std::thread;
+use std::sync::mpsc;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4567").unwrap();
+    println!("Listening {}", listener.local_addr().unwrap());
 
-    let port = listener.local_addr().unwrap().port();
+    let (broadcast_tx, broadcast_rx) = mpsc::channel::<u8>();
+    let (new_clients_tx, new_clients_rx) = mpsc::channel::<TcpStream>();
+    let mut client_txs = vec![];
 
-    println!("Listening on 127.0.0.1:{}", port);
+    thread::spawn(move || { gather_clients(listener, new_clients_tx); });
 
+    loop {
+        select! (
+            new_client = new_clients_rx.recv() => {
+                let new_client = new_client.unwrap();
+                let broadcast_tx_clone = broadcast_tx.clone();
+                let (client_tx, client_rx) = mpsc::channel();
+                client_txs.push(client_tx);
+
+                thread::spawn(move || {
+                    handle_client(new_client, broadcast_tx_clone, client_rx).unwrap();
+                });
+            },
+
+            msg = broadcast_rx.recv() => {
+                let msg = msg.unwrap();
+                for client_tx in &client_txs[..] {
+                    client_tx.send(msg).unwrap();
+                }
+            }
+        );
+    }
+}
+
+
+fn gather_clients(listener: TcpListener, chan: mpsc::Sender<TcpStream>) {
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move || {
-                    handle_client(stream)
-                });
+                chan.send(stream).unwrap();
             },
-            Err(e) => println!("Error: {}", e),
+            Err(e) => {
+                println!("Error with incoming stream: {}", e);
+            },
         }
     }
-
-    drop(listener);
 }
 
-fn handle_client(mut stream: TcpStream) -> Result<()> {
+
+fn handle_client(mut stream: TcpStream, broadcast_tx: mpsc::Sender<u8>, broadcast_rx: mpsc::Receiver<u8>) -> Result<()> {
     let client_addr = try!(stream.peer_addr());
     let reader = BufReader::new(try!(stream.try_clone()));
 
     println!("{}: connected.", client_addr);
 
-    for maybe_line in reader.lines() {
-        let line = try!(maybe_line);
-        println!("{}: {}", client_addr, line);
+    let (messages_tx, messages_rx) = mpsc::channel::<String>();
+    thread::spawn(move || { gather_client_messages(reader, messages_tx); });
 
-        match &line[..] {
-            "ping" => {
-                try!(write!(stream, "pong\n"));
+    loop {
+        select! (
+            user_msg = messages_rx.recv() => {
+                let user_msg = user_msg.unwrap();
+                println!("{}: {}", client_addr, user_msg);
+
+                match &user_msg[..] {
+                    "ping" => {
+                        try!(write!(stream, "pong\n"));
+                    },
+                    "exit" => {
+                        try!(write!(stream, "goodbye\n"));
+                        try!(stream.shutdown(net::Shutdown::Both));
+                        break
+                    },
+                    "msg" => {
+                        broadcast_tx.send(42).unwrap();
+                        try!(write!(stream, "ok\n"));
+                    },
+                    _ => {
+                        try!(write!(stream, "unknown\n"));
+                    }
+                }
             },
-            "exit" => {
-                try!(write!(stream, "goodbye\n"));
-                try!(stream.shutdown(net::Shutdown::Both));
-                break
-            },
-            _ => {
-                try!(write!(stream, "unknown\n"));
+
+            broadcast_msg = broadcast_rx.recv() => {
+                let broadcast_msg = broadcast_msg.unwrap();
+                try!(write!(stream, "broadcast: {}", broadcast_msg));
             }
-        }
+        );
     }
 
     println!("{}: disconnected.", client_addr);
-
     Ok(())
+}
+
+
+fn gather_client_messages(reader: BufReader<TcpStream>, chan: mpsc::Sender<String>) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        chan.send(line).unwrap();
+    }
 }
